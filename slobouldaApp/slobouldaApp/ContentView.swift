e4baf923e433a170e6a7ca517ef55e1e6d57f528
RichardCernansky <richard.cernansky3@gmail.com>
2023-08-15 12:04:30 +0200

    “3”

--- slobouldaApp/slobouldaApp/ContentView.swift
+++ slobouldaApp/slobouldaApp/ContentView.swift
@@ -7,59 +7,14 @@
 
 import SwiftUI
 
-import SwiftUI
-import UIKit
-
-struct UIKitBackgroundView: UIViewRepresentable {
-    
-    func offsetImage(_ image: UIImage, by offset: CGFloat) -> UIImage {
-        let newSize = CGSize(width: image.size.width, height: image.size.height + offset)
-        let renderer = UIGraphicsImageRenderer(size: newSize)
-        
-        return renderer.image { context in
-            let rect = CGRect(x: 0, y: offset, width: image.size.width, height: image.size.height)
-            image.draw(in: rect)
-        }
-    }
-
-    func makeUIView(context: Context) -> UIImageView {
-        guard let originalImage = UIImage(named: "AppIcon") else {
-            return UIImageView() // return a blank UIImageView if the image isn't found
-        }
-        
-        let zoomScale: CGFloat = 0.5  // Smaller than 1 will zoom out
-        let newSize = CGSize(width: originalImage.size.width * zoomScale, height: originalImage.size.height * zoomScale)
-        
-        let renderer = UIGraphicsImageRenderer(size: newSize)
-        let resizedImage = renderer.image { _ in
-            originalImage.draw(in: CGRect(origin: .zero, size: newSize))
-        }
-        
-        let yOffset: CGFloat = 320  // Adjust this value as needed
-        let finalImage = offsetImage(resizedImage, by: yOffset)
-        
-        let imageView = UIImageView(image: finalImage)
-        imageView.contentMode = .top // Set to top so that the offset image is shown from its top
-
-        return imageView
-    }
-    
-    func updateUIView(_ uiView: UIImageView, context: Context) {
-        // No update code necessary
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            UIKitBackgroundView() // Adjust contentScale as needed
-            
-            VStack {
-                
-                Text("Hello, world!")
-            }
-            .padding()
-        }
+        Image("backgroundImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .offset(y: -18) // This pushes the image up by 50
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
