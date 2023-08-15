1bc314014f2d1b9d7655915f207dad1fd1f97b68
RichardCernansky <richard.cernansky3@gmail.com>
2023-08-15 09:30:23 +0200

    “3”

--- sloboulda/src/main/java/com/example/demo/repository/BoulderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Boulder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoulderRepository extends JpaRepository<Boulder, Long> {
-    //all crud database methods
-
-}
---
+++ slobouldaApp/slobouldaApp/slobouldaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  slobouldaAppApp.swift
+//  slobouldaApp
+//
+//  Created by Richard Čerňanský on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct slobouldaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
