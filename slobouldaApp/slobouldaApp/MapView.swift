a99173781924b305b52d03ac338e01c9cbe66158
RichardCernansky <richard.cernansky3@gmail.com>
2023-08-18 14:47:32 +0200

    “3”

--- slobouldaApp/slobouldaApp/MapView.swift
+++ slobouldaApp/slobouldaApp/MapView.swift
@@ -11,33 +11,59 @@ import SwiftUI
 import MapKit
 
 
+struct ClimbingSpot: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let grade: String
+}
+
 
 struct MapView: View {
+    @State private var spots: [ClimbingSpot] = []
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 48.6690, longitude: 19.6990),
         span: MKCoordinateSpan(latitudeDelta: 6.0, longitudeDelta: 6.0)
-        // Adjust this for desired zoom level
     )
-    
-    
-    let locations: [Location] = [sampleLocation]
 
     var body: some View {
-        Map(coordinateRegion: $region,
-            showsUserLocation: true,
-            annotationItems: locations) { location in
-                MapMarker(coordinate: location.coordinate, tint: .blue) // This is the default pin
+        Map(coordinateRegion: $region, showsUserLocation: true, annotationItems: spots) { spot in
+            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: spot.latitude, longitude: spot.longitude)) {
+                Image("boulder")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 45, height: 45) // Adjust size as needed
+            }
         }
+        .edgesIgnoringSafeArea(.all)
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "http://localhost:8080/api/v1/boulders") else {
+            print("Invalid URL")
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode([ClimbingSpot].self, from: data) {
+                    DispatchQueue.main.async {
+                        self.spots = decodedResponse
+                    }
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
 
-let sampleLocation = Location(coordinate: CLLocationCoordinate2D(latitude: 48.733, longitude: 18.933))
 
-struct Location: Identifiable {
-    let id = UUID()
-    let coordinate: CLLocationCoordinate2D
-}
+
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
