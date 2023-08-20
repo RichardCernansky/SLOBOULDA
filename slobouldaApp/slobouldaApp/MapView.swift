d6c47f5bb0a10c4cf270983eadec912d25dabace
RichardCernansky <richard.cernansky3@gmail.com>
2023-08-20 21:18:58 +0200

    “3”

--- slobouldaApp/slobouldaApp/MapView.swift
+++ slobouldaApp/slobouldaApp/MapView.swift
@@ -11,58 +11,84 @@ import SwiftUI
 import MapKit
 
 
-struct ClimbingSpot: Codable, Identifiable {
+struct Boulder: Codable, Identifiable {
     let id: Int
     let name: String
     let latitude: Double
     let longitude: Double
     let grade: String
+    let boulderAreaId: Int?
 }
 
+struct BoulderArea: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let boulders: [Boulder]
+}
+
+struct LocationAnnotation: Identifiable {
+    var id = UUID() // This gives each annotation a unique identifier
+    var coordinate: CLLocationCoordinate2D
+    var isBoulder: Bool
+}
 
 struct MapView: View {
-    @State private var spots: [ClimbingSpot] = []
+    @State private var boulders: [Boulder] = []
+    @State private var boulderAreas: [BoulderArea] = []
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 48.6690, longitude: 19.6990),
         span: MKCoordinateSpan(latitudeDelta: 6.0, longitudeDelta: 6.0)
     )
 
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true, annotationItems: spots) { spot in
-            MapAnnotation(coordinate: CLLocationCoordinate2D(latitude: spot.latitude, longitude: spot.longitude)) {
-                Image("boulder")
+         Map(coordinateRegion: $region, showsUserLocation: true, annotationItems: annotations) { location in
+            MapAnnotation(coordinate: location.coordinate) {
+                Image(location.isBoulder ? "boulder" : "boulderArea")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 45, height: 45) // Adjust size as needed
             }
         }
         .edgesIgnoringSafeArea(.all)
-        .onAppear(perform: loadData)
+        .onAppear{loadData(from: "boulder_areas", into: $boulderAreas)
+                  loadData(from: "boulders?boulderAreaId=0", into: $boulders)
+            
+        }
     }
     
-    func loadData() {
-        guard let url = URL(string: "http://localhost:8080/api/v1/boulders") else {
+    var annotations: [LocationAnnotation] {
+        var locations: [LocationAnnotation] = []
+        
+        for area in boulderAreas {
+            if let boulder = area.boulders.first {
+                locations.append(LocationAnnotation(coordinate: CLLocationCoordinate2D(latitude: boulder.latitude, longitude: boulder.longitude), isBoulder: false))
+            }
+        }
+        
+        for boulder in boulders {
+            locations.append(LocationAnnotation(coordinate: CLLocationCoordinate2D(latitude: boulder.latitude, longitude: boulder.longitude), isBoulder: true))
+        }
+        
+        return locations
+    }
+    
+    func loadData<T: Decodable & Identifiable>(from tableEndpoint: String, into binding: Binding<[T]>) {
+        guard let url = URL(string: "http://localhost:8080/api/v1/\(tableEndpoint)") else {
             print("Invalid URL")
             return
         }
-        let request = URLRequest(url: url)
         
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                if let decodedResponse = try? JSONDecoder().decode([ClimbingSpot].self, from: data) {
-                    DispatchQueue.main.async {
-                        self.spots = decodedResponse
-                    }
-                    return
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let decodedResponse = try? JSONDecoder().decode([T].self, from: data) {
+                DispatchQueue.main.async {
+                    binding.wrappedValue = decodedResponse
                 }
+            } else {
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
             }
-            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
         }.resume()
     }
 }
-
-
-
 
 
 struct MapView_Previews: PreviewProvider {
